9ef6863884d27aad11fc532cff00cf1b39a48203
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-18 19:41:30 +0200

    add registration forms

--- twitter_django_react/twitter_django_react/urls.py
+++ twitter_django_react/twitter_django_react/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,6 +24,7 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('blog/', include('blog.urls')),
     path('library/', include('library.urls')),
-    path('clinic/', include('clinic.urls'))
+    path('clinic/', include('clinic.urls')), 
+    path('register/', user_views.register, name='register')
     
 ]
